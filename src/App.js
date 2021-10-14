67d0d028bec11eec74220275073b961a1c5c1320
Luke Landau <luke.a.landau@gmail.com>
2021-10-14 18:36:56 +0100

    Duplicate from not adding app.js to commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import TitleSection from "./Components/TitleSection/TitleSection";
 import ProblemSection from "./Components/ProblemSection/ProblemSection";
+import SiteNav from "./Components/SiteNav/SiteNav";
 
 function App() {
   return (
-      <div>
-        <TitleSection />
-        <ProblemSection />
-      </div>
+      <main>
+          <header>
+              <SiteNav />
+          </header>
+          <div>
+            <TitleSection />
+            <ProblemSection />
+          </div>
+      </main>
   )
 }
 
