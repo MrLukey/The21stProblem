750765c359c645687b39ee6a61345634bc9a04ae
Luke Landau <luke.a.landau@gmail.com>
2021-10-14 17:53:00 +0100

    Setup React project and and rebuilt title section as component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import TitleSection from "./Components/TitleSection/TitleSection";
+
+function App() {
+  return (
+      <TitleSection />
+  )
+}
+
+export default App;
---
+++ src/Components/TitleSection/TitleSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './TitleSection.css'
+
+const TitleSection = () => {
+    return (
+        <section id="titleSection">
+            <div className="titleWrapper">
+                <h1 className="mainTitle">The 21<sup>st</sup> Problem</h1>
+                <h2 className="mainTitle">Ensuring Security and Prosperity in a Rapidly Changing World</h2>
+                <h3 className="mainTitle">WIP</h3>
+            </div>
+            <i className="gg-chevron-double-down"></i>
+        </section>
+    )
+}
+
+export default TitleSection
