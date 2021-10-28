38e3d7a15ed4ef84c62f7ffe3867e4a77f7ff448
Luke Landau <luke.a.landau@gmail.com>
2021-10-28 16:39:35 +0100

    Added validation to first and last name inputs

--- client/src/Components/Inputs/FirstAndLastNameInput/FirstAndLastNameInput.js
+++ client/src/Components/Inputs/FirstAndLastNameInput/FirstAndLastNameInput.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import {FormGroup} from "react-bootstrap";
 
 const FirstAndLastNameInput = (props) => {
 
+    const [firstNameValid, setFirstNameValid] = useState('')
+    const [secondNameValid, setSecondNameValid] = useState('')
+
     const handleFirstNameInput = evt => {
-        if (evt.target.value.length <= 30){
-            props.setFirstName(evt.target.value)
+        const name = evt.target.value.replaceAll(/[^A-Za-z ']/g, '')
+        if (name.length <= 35) {
+            props.setFirstName(name)
+        }
+        if (name.length > 0 && name.length <= 35){
+            setFirstNameValid(' is-valid')
+        } else {
+            setFirstNameValid('')
         }
     }
 
     const handleSecondNameInput = evt => {
-        if (evt.target.value.length <= 30){
-            props.setLastName(evt.target.value)
+        const name = evt.target.value.replaceAll(/[^A-Za-z ']/g, '')
+        if (name.length <= 35) {
+            props.setLastName(name)
+        }
+        if (name.length > 0 && name.length <= 35){
+            setSecondNameValid(' is-valid')
+        } else {
+            setSecondNameValid('')
         }
     }
 
     return (
         <div className="d-flex flex-row flex-nowrap mb-2">
             <FormGroup className="form-floating flex-grow-1 mx-1">
-                <input className="form-control" type="text" id="firstName" value={props.firstName} onChange={handleFirstNameInput} maxLength={30}/>
+                <input className={"form-control" + firstNameValid} type="text" id="firstName" value={props.firstName} onChange={handleFirstNameInput} maxLength={35}/>
                 <label htmlFor="firstName">First name</label>
             </FormGroup>
             <FormGroup className="form-floating flex-grow-1 mx-1">
-                <input className="form-control" type="text" id="lastName" value={props.lastName} onChange={handleSecondNameInput} maxLength={30}/>
+                <input className={"form-control" + secondNameValid} type="text" id="lastName" value={props.lastName} onChange={handleSecondNameInput} maxLength={35}/>
                 <label htmlFor="lastName">Last name</label>
             </FormGroup>
         </div>
