856e9a4393eb78b33772387d38b6d081ff6edc1a
Luke Landau <luke.a.landau@gmail.com>
2021-10-28 16:12:41 +0100

    Added validation to residence input

--- client/src/Components/Inputs/ResidenceInput/ResidenceInput.js
+++ client/src/Components/Inputs/ResidenceInput/ResidenceInput.js
@@ -1,7 +1,9 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {FormGroup} from "react-bootstrap";
 
 const ResidenceInput = (props) => {
+
+    const [inputValid, setInputValid] = useState('')
 
     useEffect(() => {
         const url = 'http://localhost:3001/get-all-countries'
@@ -20,7 +22,9 @@ const ResidenceInput = (props) => {
         const countryMatches = props.allCountries.filter(country => country.name.toLowerCase().includes(evt.target.value.toLowerCase()))
         if (countryMatches.length === 1){
             props.setPlaceOfResidence(countryMatches[0].name)
+            setInputValid(' is-valid')
         } else {
+            setInputValid('')
             countryMatches.forEach(country => {
                 if (country.name.toLowerCase().includes(evt.target.value.toLowerCase())){
                     props.setPlaceOfResidence(evt.target.value)
@@ -29,8 +33,8 @@ const ResidenceInput = (props) => {
     }
 
     return (
-        <FormGroup className="form-floating flex-grow-1 mx-1">
-            <input className="form-control" type="search" list="countryOfResidence" value={props.placeOfResidence} onChange={handleResidenceInput}/>
+        <FormGroup className="form-floating flex-grow-1 mx-1 ">
+            <input className={"form-control" + inputValid} type="search" list="countryOfResidence" value={props.placeOfResidence} onChange={handleResidenceInput}/>
             <datalist id="countryOfResidence">
                 {props.allCountries.map(country =>
                     <option key={country.id}>{country.name}</option>
