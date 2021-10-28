29cba83eca8ff86ddd7f9d1770fa23ac87a9d090
Luke Landau <luke.a.landau@gmail.com>
2021-10-28 19:08:42 +0100

    Added validation to profession selector

--- client/src/Components/Inputs/ProfessionInput/ProfessionInput.js
+++ client/src/Components/Inputs/ProfessionInput/ProfessionInput.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {FormGroup} from "react-bootstrap";
 
 const ProfessionInput = (props) => {
+
+    const [professionValid, setProfessionValid] = useState('')
 
     const handleProfessionSelectInput = evt => {
         const selected = evt.target.value
@@ -15,10 +17,15 @@ const ProfessionInput = (props) => {
             props.setProfessionDetailsText('')
             props.setProfessionDetails('')
         }
+        if (selected !== 'Other' && selected !== ''){
+            setProfessionValid(' is-valid')
+        } else {
+            setProfessionValid('')
+        }
     }
     return (
         <FormGroup className="form-floating flex-grow-1 mx-1" value={props.profession} onChange={handleProfessionSelectInput}>
-            <select className="form-control" id="profession">
+            <select className={"form-control" + professionValid} id="profession">
                 <option key={0} value=""> </option>
                 <option key={1}>Scientist</option>
                 <option key={2}>Engineer</option>
