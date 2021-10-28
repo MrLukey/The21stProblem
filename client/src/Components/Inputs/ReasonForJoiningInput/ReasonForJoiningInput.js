fce9c3c10727784787af5394ec443e6dbce7e5be
Luke Landau <luke.a.landau@gmail.com>
2021-10-28 19:43:10 +0100

    Added validation to reason for sign up

--- client/src/Components/Inputs/ReasonForJoiningInput/ReasonForJoiningInput.js
+++ client/src/Components/Inputs/ReasonForJoiningInput/ReasonForJoiningInput.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import {FormGroup} from "react-bootstrap";
 
 const ReasonForJoiningInput = props => {
 
+    const [reasonValid, setReasonValid] = useState('')
+
     const handleReasonForJoiningInput = evt => {
-        if (evt.target.value.length <= 500){
-            props.setReasonForJoining(evt.target.value)
+        const regex = new RegExp(/[^\p{L}'’` ]+/ug)
+        const reasonForJoining = evt.target.value.replaceAll(regex, '')
+        if (reasonForJoining.length <= 500) {
+            props.setReasonForJoining(reasonForJoining.charAt(0).toUpperCase() + reasonForJoining.slice(1))
+        }
+        if (reasonForJoining.length > 0 && reasonForJoining.length <= 500){
+            setReasonValid(' is-valid')
+        } else {
+            setReasonValid('')
         }
     }
 
     return (
         <FormGroup className="form-floating mx-1 mb-2" >
-                    <textarea className="form-control h-auto"  rows={5} id="comment" value={props.reasonForJoining}
+                    <textarea className={"form-control h-auto" + reasonValid}  rows={5} id="comment" value={props.reasonForJoining}
                               onChange={handleReasonForJoiningInput} maxLength={500} />
             <label htmlFor="comment">Reason for joining</label>
         </FormGroup>
