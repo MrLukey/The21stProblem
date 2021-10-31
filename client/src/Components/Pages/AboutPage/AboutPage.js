657e40b5b3f347811f07e086808d5524ddb3c3b4
Luke Landau <luke.a.landau@gmail.com>
2021-10-31 15:09:36 +0000

    Added some content to about page - needs rework (brain not working)

--- client/src/Components/Pages/AboutPage/AboutPage.js
+++ client/src/Components/Pages/AboutPage/AboutPage.js
@@ -3,7 +3,14 @@ import React from "react";
 const AboutPage = () => {
     return (
         <section className="full-page d-flex flex-column flex-nowrap justify-content-center align-items-center bg-dark">
-            <h3 className="card-title text-light text-muted text-center mb-3">About</h3>
+            <div className="container text-center col-lg-6">
+                <h3 className="card-title text-light text-muted text-center mb-3">About</h3>
+                <p className="card-text text-light">Started by a cadre of scientists and engineers from Extinction
+                    Rebellion, The 21<sup>st</sup> Problem aims to highlight the scale and severity of the climate crisis.
+                    Instead of sugar-coating the situation to preserve false hope, we believe assuming the worst and acting
+                    accordingly is the most prudent course of action.
+                </p>
+            </div>
         </section>
     )
 }
