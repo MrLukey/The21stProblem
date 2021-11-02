30cb4042942e116088128dd4504c890697b48232
Luke Landau <luke.a.landau@gmail.com>
2021-11-02 13:57:30 +0000

    Simplified cover page to avoid long load times

--- client/src/Components/Pages/CoverPage/CoverPage.js
+++ client/src/Components/Pages/CoverPage/CoverPage.js
@@ -1,8 +1,5 @@
 import React, {useEffect} from "react"
-import Button from "react-bootstrap/Button";
-import "./CoverPage.css"
-import "./forestFire.jpg"
-import "./fire.jpg"
+import CallToActionHero from "../../Heroes/CallToActionHero";
 
 const CoverPage = () => {
 
@@ -21,13 +18,16 @@ const CoverPage = () => {
     }, [])
 
     return (
-        <div className="full-page d-flex flex-column justify-content-center align-items-center text-center p-5 text-light" id="cover">
-            <main role="main">
-                <h1 className="mb-2 fw-bold">The 21<sup>st</sup> Problem</h1>
-                <h2 className="mb-2 lead">Ensuring Security and Prosperity in a Rapidly Changing World</h2>
-                <h3 className="mb-5 fw-bold">WIP</h3>
-                <Button variant="light" href="/problem">Continue</Button>
-            </main>
+        <div className="full-page bg-dark d-flex align-items-center">
+            <CallToActionHero
+                title="The 21st Problem"
+                info="Ensuring Security and Prosperity in a Rapidly Changing World"
+                buttonOneText="About"
+                buttonOneVariant="secondary"
+                buttonOneHref="about"
+                buttonTwoText="Enter"
+                buttonTwoVariant="light"
+                buttonTwoHref="problem" />
         </div>
     )
 }
