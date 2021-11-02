3a9320fd794cb6497c0f8a13daa229a332f9103a
Luke Landau <luke.a.landau@gmail.com>
2021-11-02 23:42:45 +0000

    Made variable names unified and semantic

--- client/src/Components/Inputs/FirstAndLastNameInput/FirstAndLastNameInput.js
+++ client/src/Components/Inputs/FirstAndLastNameInput/FirstAndLastNameInput.js
@@ -16,16 +16,16 @@ const FirstAndLastNameInput = (props) => {
         }
     }
 
-    const handleSecondNameInput = (evt) => {
+    const handleLastNameInput = (evt) => {
         const regex = new RegExp(/[^\p{L}'’` ]+/ug)
         const name = evt.target.value.replaceAll(regex, '')
         if (name.length <= 35) {
             props.setLastName(name.charAt(0).toUpperCase() + name.slice(1))
         }
         if (name.length > 0 && name.length <= 35) {
-            props.setSecondNameValid(' is-valid')
+            props.setLastNameValid(' is-valid')
         } else {
-            props.setSecondNameValid('')
+            props.setLastNameValid('')
         }
     }
 
@@ -36,7 +36,7 @@ const FirstAndLastNameInput = (props) => {
                 <label htmlFor="firstName">First name</label>
             </FormGroup>
             <FormGroup className="form-floating flex-grow-1 mx-1">
-                <input className={"form-control" + props.secondNameValid} type="text" id="lastName" value={props.lastName} onChange={handleSecondNameInput} maxLength={35} />
+                <input className={"form-control" + props.lastNameValid} type="text" id="lastName" value={props.lastName} onChange={handleLastNameInput} maxLength={35} />
                 <label htmlFor="lastName">Last name</label>
             </FormGroup>
         </div>
